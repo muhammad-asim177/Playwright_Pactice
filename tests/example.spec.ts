a034516bd7b420b6743eac5aab3bf3436169c1e7
muhammad-asim177 <muhammad.asimsqa177@gmail.com>
2026-07-16 22:45:51 +0500

    new test added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,3 +19,9 @@ test('get started link', async ({ page }) => {
 });
 
 
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
